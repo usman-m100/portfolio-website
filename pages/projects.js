e9f747fbf6182ea98b276bf8331560cb63eae7e0
Usman <111375580+usman-m100@users.noreply.github.com>
2022-11-05 22:34:49 +0000

    Add files via upload

--- pages/projects.js
+++ pages/projects.js
@@ -2,6 +2,7 @@ import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { ProjectGridItem } from '../components/grid-item'
 import thumbGameEngine from '../public/images/game-engine.png'
+import thumbtodo from '../public/images/todo1.png'
 import Layout from '../components/layouts/article'
 
 const Projects = () => {
@@ -15,10 +16,13 @@ const Projects = () => {
                 <SimpleGrid columns={[1, 1, 2]} gap={6}>
                     <Section>
                         <ProjectGridItem id="gameengine" title="Wave Engine" thumbnail={thumbGameEngine}>
-                            Wave Game Engine - made with C++ (Work in progress)
+                            Wave Game Engine - made with C++
                         </ProjectGridItem>
                     </Section>
                     <Section>
+                        <ProjectGridItem id="todo" title="ReactJS Todo App" thumbnail={thumbtodo}>
+                            Todo Application - Made with React
+                        </ProjectGridItem>
                     </Section>
                 </SimpleGrid>
             </Container>
